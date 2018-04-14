3831bd09c3e7e4468a6bd6ef5134f6e4458ca64e
Joseph Birkner <joseph.birkner@gmail.com>
2018-04-14 18:54:59 +0200

    Work on fistbump script

--- ros_bump_proxy/runroscore.sh
+++ ros_bump_proxy/runroscore.sh
@@ -1,2 +1,6 @@
-export ROS_IP=$0
-export 
+
+export ROS_IP=10.42.0.1 # make sure you are on the same network as Roboy
+export ROS_HOSTNAME=$ROS_IP
+export ROS_MASTER_URI=http://$ROS_IP:11311
+
+# roscore &
