f1afcd2fa62b334dba0978475caa036050f69d4d
Joseph Birkner <joseph.birkner@gmail.com>
2018-04-15 16:12:11 +0200

    hotfix

--- ros_bump_proxy/run.py
+++ ros_bump_proxy/run.py
@@ -5,30 +5,30 @@ import flask as fl
 
 # Initializes a rospy node so that the SimpleActionClient can
 # publish and subscribe over ROS.
-try:
 import rospy
 from std_msgs.msg import String
 import actionlib
 from roboy_communication_control.msg import PerformMovementAction, PerformMovementGoal
+try:
     rospy.init_node('fistbump_client_py')
-    faceTopic = rospy.Publisher(
-        '/roboy/cognition/face/show_emotion',
-        String, queue_size=10)
-    handTopic = rospy.Publisher(
-        '/roboy/middleware/FingerCommand',
-        String, queue_size=10)
+    #faceTopic = rospy.Publisher(
+    #   '/roboy/cognition/face/show_emotion',
+    #    String, queue_size=10)
+    #handTopic = rospy.Publisher(
+    #   '/roboy/middleware/FingerCommand',
+    #   String, queue_size=10)
     actionClient = actionlib.SimpleActionClient(
-        'shoulder_left_movement_server',
-        PerformMovementAction)
+       'shoulder_left_movement_server',
+       PerformMovementAction)
     print("Waiting for 'shoulder_left_movement_server'...")
     actionClient.wait_for_server()
     print(" ...done.")
-    print("Setting hand default position...")
-    handTopic.publish(String("index: 0, angles: [0, 0, 0, 0]"))
-    handTopic.publish(String("index: 1, angles: [0, 0, 0, 0]"))
-    handTopic.publish(String("index: 2, angles: [0, 0, 0, 0]"))
-    handTopic.publish(String("index: 3, angles: [0, 0, 0, 0]"))
-    handTopic.publish(String("index: 4, angles: [0, 0, 0, 0]"))
+    # print("Setting hand default position...")
+    #handTopic.publish(String("index: 0, angles: [0, 0, 0, 0]"))
+    #handTopic.publish(String("index: 1, angles: [0, 0, 0, 0]"))
+    #handTopic.publish(String("index: 2, angles: [0, 0, 0, 0]"))
+    #handTopic.publish(String("index: 3, angles: [0, 0, 0, 0]"))
+    #handTopic.publish(String("index: 4, angles: [0, 0, 0, 0]"))
 except rospy.ROSInterruptException:
     print("program interrupted before completion", file=sys.stderr)
 
@@ -41,15 +41,15 @@ def hello():
 
 @app.route("/bump", methods=['GET', 'POST'])
 def makeFistbumpAndEmotion():
-    global faceTopic, actionClient, handTopic
-    faceTopic.publish(String("emotion: 'shy'"))
+    global actionClient #, handTopic
+    #faceTopic.publish(String("emotion: 'shy'"))
     bumpGoal = PerformMovementGoal(action = "shoulder_left_fistbump3")
     actionClient.send_goal(bumpGoal)
-    handTopic.publish(String("index: 0, angles: [40, 40, 40, 40]"))
-    handTopic.publish(String("index: 1, angles: [40, 40, 40, 40]"))
-    handTopic.publish(String("index: 2, angles: [40, 40, 40, 40]"))
-    handTopic.publish(String("index: 3, angles: [40, 40, 40, 40]"))
-    handTopic.publish(String("index: 4, angles: [40, 40, 40, 40]"))
+    #handTopic.publish(String("index: 0, angles: [40, 40, 40, 40]"))
+    #handTopic.publish(String("index: 1, angles: [40, 40, 40, 40]"))
+    #handTopic.publish(String("index: 2, angles: [40, 40, 40, 40]"))
+    #handTopic.publish(String("index: 3, angles: [40, 40, 40, 40]"))
+    #handTopic.publish(String("index: 4, angles: [40, 40, 40, 40]"))
     actionClient.wait_for_result()
     return ', '.join([str(n) for n in client.get_result().sequence])
 
