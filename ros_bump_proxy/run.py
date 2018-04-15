ca033fe7d9cb38da97bed2cc7abbd5e38e300860
Joseph Birkner <j.birkner@klebert-engineering.de>
2018-04-15 11:16:38 +0200

    Minor cleanup is ros_bump_proxy

--- ros_bump_proxy/run.py
+++ ros_bump_proxy/run.py
@@ -1,12 +1,19 @@
 
 #! /usr/bin/env python
-
 from __future__ import print_function
+import flask as fl
 import rospy
 import actionlib
-from roboy_communication_control.msg import PerformMovementAction, PerformMovementGoal
-import pdb
+##from roboy_communication_control.msg import PerformMovementAction, PerformMovementGoal
 
+app = fl.Flask(__name__)
+
+@app.route("/hi", methods=['GET', 'POST'])
+def hello():
+    print("Helle there!")
+    return "Hello there!"
+
+@app.route("/bump", methods=['GET', 'POST'])
 def makeFistbump():
     client = actionlib.SimpleActionClient(
         'shoulder_left_movement_server',
@@ -14,12 +21,8 @@ def makeFistbump():
     # pdb.set_trace()
     print("Waiting for Server")
     client.wait_for_server()
-    bumpAction = PerformMovementGoal(action="shoulder_left_gogogo")
-
-    # bumpAction.action_goal = "shoulder_left_gogogo"
-    # pdb.set_trace()
-    # bumpAction["action"] = "shoulder_left_gogogo"
-    client.send_goal(bumpAction)
+    bumpGoal = PerformMovementGoal(action = "shoulder_left_gogogo")
+    client.send_goal(bumpGoal)
     client.wait_for_result()
     return client.get_result()
 
@@ -27,9 +30,7 @@ if __name__ == '__main__':
     try:
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
-        rospy.init_node('fistbump_client_py')
-        result = makeFistbump()
-        print("Result:", ', '.join([str(n) for n in result.sequence]))
+        ##rospy.init_node('fistbump_client_py')
+        app.run(host="0.0.0.0", port=5000)
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
-
